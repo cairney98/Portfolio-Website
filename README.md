aae1c149e7124b3a0c4a869521fc3abc2a9bdc33
cairney98 <mcairney92@gmail.com>
2021-11-24 10:15:37 +0000

    Complete overhaul of new files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio-Website
-
-My attempt to make my first Web Development portfolio while using GitHub in order to get more familiar with tracking code etc.
--- app.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function (e) {
-  const target = document.querySelector(".header");
-  var scrolled = window.pageYOffset;
-  var rate = scrolled * 0.8;
-
-  target.style.transform = `translate3d(0px, ${rate}px, 0px)`;
-});
